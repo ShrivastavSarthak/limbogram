8a2617d181c680a1672db197e087dbd1d11be8e5
ShrivastavSarthak <sharthakshrivastav20112002@gmail.com>
2023-04-07 20:25:34 +0530

    backup code before uploading img

--- server/routes/activity.js
+++ server/routes/activity.js
@@ -12,10 +12,14 @@ router.get('/activity',fetchuser ,async (req,res)=>{
 
 }) 
 
-// ROUTE 2: ADD activities using :POST 
+// ROUTE 2: ADD activities using :POST ------>Login requirements
 
-router.post("/post",fetchuser, async (req, res)=>{
-    const posts = await Post.find({user:req.user.id})
+router.post("/addpost",fetchuser, async (req, res)=>{
+    // If there is any kind of error
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
     res.json(posts)
 })
 
